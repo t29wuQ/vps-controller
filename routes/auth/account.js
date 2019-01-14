9ace5ea339472cd27e204753c91f90fec486ca4f
K.Tsukimoto <t.29.wuq@gmail.com>
2019-01-14 16:50:04 +0900

    :sparkles: アカウント作成時にユーザーの機器データベースを作成

--- routes/auth/account.js
+++ routes/auth/account.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const mongose = require('mongoose');
 const User = require('../../models/user');
+const UserMachine = require('../../models/machine');
 
 router.get('/', function(req, res){
     res.render("account.ejs");
@@ -15,15 +16,21 @@ router.post('/signup', function(req, res){
     User.find({"userid" : userid}, function(err, result){
         if (err)
             console.log(err);
-        if (result.length == 0){
+        if (result.length == 0){ 
             console.log("register");
             let user = new User();
             user.userid = userid;
             user.password = password;
             user.save(function(err){
                 if (err)
-                    console.log(err);
+                    res.send(err);
+                let userMachine = new UserMachine();
+                userMachine.userid = userid;
+                userMachine.save(function(err){
+                    if (err)
+                        res.send(err);
                     res.send("new created account");
+                });
             });
         } else {
             res.send('already userid');
