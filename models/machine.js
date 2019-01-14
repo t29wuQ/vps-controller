a5a6e0c262973dbcb9abdb0f5f01c2400df8038e
K.Tsukimoto <t.29.wuq@gmail.com>
2019-01-14 15:21:56 +0900

    :sparkles: ユーザーが管理する機器のスキーマを作成

---
+++ models/machine.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let UserMachine = new Schema({
+    userid : {type: String, require: true, unique: true},
+    machines : [{
+        id : {type: String, require: true, unique: true},
+        name : {type: String, require: true},
+        type : {type: String, require: true},
+        x : {type: Number, require: true},
+        y : {type: Number, require: true},
+        port : [{
+            id : {type: String, require: true},
+            name : {type: String, require: true},
+            access_point : {type: String, require: true},
+            x : {type: Number, require: true},
+            y : {type: Number, require: true},
+            ip_address : {type: String},
+            mac_address : {type: String}
+        }]
+    }]
+});
+
+module.exports = mongoose.model('usermachine', UserMachine);
+
