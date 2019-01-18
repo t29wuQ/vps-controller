f7807f0e2b82e6cddbd59c588ebd765244ca42a9
K.Tsukimoto <t.29.wuq@gmail.com>
2019-01-19 04:39:58 +0900

    :sparkles: ルータをopenvswitchからnetwork namespaceを使うようにした

--- routes/main/machine.js
+++ routes/main/machine.js
@@ -3,45 +3,53 @@ const router = express.Router();
 const UserMachine = require('../../models/machine');
 const exec = require('child_process').exec;
 const execPath = require('path').resolve(__dirname, './exec.sh');
-const randomID = require('../../crypto/random_id');
+require('date-utils');
 
-//ルータの追加
+const INITIAL_NUMBER_OF_PORTS = 3; //ルーター作成時の初期ポート数
+
+/**
+ * エラー文のjsonを返す
+ * @param {String} error 
+ */
+const getErrorJson = function(error){
+    return {status: 0, log: error};
+}
+
+/**
+ * ルーターの追加
+ */
 router.post('/router/add', function(req, res){
-    
     let userid = req.session.userid;
     
     UserMachine.find({"userid" : userid}, function(error, result){
         if (error)
-            res.json(getErrorJson(error));
+            return res.json(getErrorJson(error));
         let userMachine = result[0];
-        let name = userid + userMachine.machines.length;
-        let id = (String(userMachine.usernumber) + String(userMachine.machines.length) + "0000000000000000").slice(0, 16);
+        let name = userid + new Date().toFormat("YYYYMMDDHH24MISS") + "_router";
         let type = "router";
         let x = req.body.x;
         let y = req.body.y;
-        exec(execPath + " 'ovs-vsctl add-br " + name + "'", function(error, stdout, stderr){
+        exec(execPath + " 'ip netns add " + name + "'", function(error, stdout, stderr){
             if (error)
-                res.json(getErrorJson(error));
-            console.log("ovs-vsctl add-br " + name + " stdout: " + stdout);
-            console.log("ovs-vsctl add-br " + name + " stderr: " + stderr);
-            exec(execPath + " 'ovs-vsctl set bridge " + name + " other-config:datapath-id=" + id + "'", function(error, stdout, stderr){
+                return res.json(getErrorJson(error));
+            let router = { name: name, display_name: "router", type: type, x: x, y: y };
+            userMachine.machines.push(router);
+            let ports = []
+            for (i = 0;i < INITIAL_NUMBER_OF_PORTS;i++){
+                let port = {status: 0, name: "", open_port_number: i, ip_address: "", mac_address: ""};
+                userMachine.machines.push(port);
+                ports.push(port)
+            }
+            userMachine.save(function(error){
+                console.log(error);
                 if (error)
-                    res.json(getErrorJson(error));
-                console.log("ovs-vsctl set bridge " + name + " other-config:datapath-id=" + id + " stdout: " + stdout);
-                console.log("ovs-vsctl set bridge " + name + " other-config:datapath-id=" + id + " stderr: " + stderr);
-                let router = { id : id, name : name, type : "router", x : x, y : y};
-                result[0].machines.push(router);
-                result[0].save(function(error){
-                    if (error)
-                        res.json(getErrorJson(error));
-                    res.json({
-                        status: 1,
-                        name: name,
-                        id: id,
-                        log: "success: add router on your topology"
-                    });
+                    return res.json(getErrorJson(error));
+                return res.json({
+                    status: 1,
+                    name: name,
+                    port: ports,
+                    log: "success: add router on your topology"
                 });
-                
             });
         });
     });
@@ -140,9 +148,5 @@ router.post('/veth/add', function(req, res){
 router.post('/veth/del', function(req, res){
 
 });
-
-const getErrorJson = function(error){
-    return {status: 0, log: error};
-}
 
 module.exports = router;
