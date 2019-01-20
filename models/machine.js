9957f8eade79f042c75d8fe0822b7179f97f4b86
K.Tsukimoto <t.29.wuq@gmail.com>
2019-01-21 02:34:49 +0900

    :floppy_disk: モデルを再修正

--- models/machine.js
+++ models/machine.js
@@ -4,29 +4,27 @@ const Schema = mongoose.Schema;
 let UserMachine = new Schema({
     userid : {type: String, require: true, unique: true}, //ログイン用のuserid
     usernumber : {type : Number, require: true, unique: true}, //内部用のuserid
+    if_count : {type : Number, require : true}, //IF名の重複を防ぐためのカウント
     machines : [{
-        name : {type: String, require: true, unique: true}, //機器の名称
+        open_machine_id : {type : String, require: true}, //公開用の機器ID
+        name : {type: String, require: true}, //機器の実名称
         display_name : {type: String, require: true}, //表示用の名称
         description : {type : String}, //機器の説明
         type : {type: String, require: true}, //機器の種類 ex)ルータ スイッチ
         x : {type: Number, require: true}, //canvas上のx座標
         y : {type: Number, require: true}, //canvas上のy座標
-        port : [{
+        ports : [{
             status: {type: Number, require: true}, //0, 設定が無効 1, 接続がされていない 2, 接続中
-            name : {type: String, require: true}, //内部ポート名
+            open_port_id : {type: String, require: true}, //公開用ポートID
+            port_name : {type: String, require: true}, //実ポート名
             open_port_number : {type: Number, require: true}, //公開用のポート番号
+            opposed_open_port_id: {type: String, require: true}, //対向ポートid
+            opposed_port_name :  {type: String, require: true}, //対向実ポート名
             ip_address : {type: String}, //ポートipアドレス
-            mac_address : {type: String} //ポートmacアドレス
+            mac_address : {type: String}, //ポートmacアドレス
+            x : {type: Number, require: true}, //canvas描画時の線のx座標
+            y : {type: Number, require: true} //canvas描画時の線のy座標
         }]
-    }],
-    veth : [{
-        port1 : {type: String, require: true}, //接続先ポート
-        port2 : {type: String, require: true}, //接続先ポート
-        name : {type: String, require: true}, //veth名
-        begin_x : {type: Number, require: true}, //canvas描画時の線の始点x座標
-        begin_y : {type: Number, require: true}, //canvas描画時の線の始点y座標
-        end_x : {type: Number, require: true}, //canvas描画時の線の終点x座標
-        end_y : {type: Number, require: true}, //canvas描画時の線の終点y座標
     }]
 });
 
