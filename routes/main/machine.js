aeca7b3bdb3e10eccfc5f5858b1ef8daa0552927
K.Tsukimoto <t.29.wuq@gmail.com>
2019-01-23 01:03:49 +0900

    :recycle: DBをMySQLに変更したので総書き換え

--- routes/main/machine.js
+++ routes/main/machine.js
@@ -3,57 +3,63 @@ const router = express.Router();
 const UserMachine = require('../../models/machine');
 const exec = require('child_process').exec;
 const execPath = require('path').resolve(__dirname, './exec.sh');
+const queryExec = require("../../query_exec");
 require('date-utils');
 
-const INITIAL_NUMBER_OF_PORTS_OF_ROUTER = 3; //ルーター作成時の初期ポート数
-const INITIAL_NUMBER_OF_PORTS_OF_SWITCH = 6; //スイッチ作成時の初期ポート数
 
 /**
  * エラー文のjsonを返す
  * @param {String} error 
  */
 const getErrorJson = function(error){
-    return {status: 0, log: error};
+    return {status: 1, log: error};
 }
+
+/**
+ * shellコマンドを実行する
+ * @param {String} command 実行コマンド
+ */
+const execShellCommand = function(command){
+    return new Promise(function(resolve, reject){
+        exec(execPath + " " + command, function(error, stdout, stderr){
+            if (error){
+                console.log(command + " execShellCommand Error: " + error);
+                reject(new Error(error));
+                return;
+            }
+            resolve({stdout: stdout, stderr: stderr});
+        });
+    })
+}
+
 
 /**
  * ルーターの追加
  */
-router.post('/router/add', function(req, res){
-    let userid = req.session.userid;
-    
-    UserMachine.find({"userid" : userid}, function(error, result){
-        if (error)
-            return res.json(getErrorJson(error));
-        let userMachine = result[0];
-        let name = userid + new Date().toFormat("YYYYMMDDHH24MISS") + "_router";
-        let type = "router";
-        let x = req.body.x;
-        let y = req.body.y;
-        exec(execPath + " 'ip netns add " + name + "'", function(error, stdout, stderr){
-            if (error)
-                return res.json(getErrorJson(error));
-            let router = { name: name, display_name: "router", type: type, x: x, y: y };
-            userMachine.machines.push(router);
-            let ports = []
-            for (i = 0;i < INITIAL_NUMBER_OF_PORTS_OF_ROUTER;i++){
-                let port = {status: 0, name: "", open_port_number: i, ip_address: "", mac_address: ""};
-                userMachine.machines.push(port);
-                ports.push(port)
-            }
-            userMachine.save(function(error){
-                console.log(error);
-                if (error)
-                    return res.json(getErrorJson(error));
-                return res.json({
-                    status: 1,
-                    name: name,
-                    port: ports,
-                    log: "success: add router on your topology"
-                });
-            });
+router.post('/router/add', async function(req, res){
+    try{
+        const user_id_search = await queryExec("select login_id, user_id from user where login_id='"+req.session.login_id+"';")
+        const user_id = user_id_search[0].user_id;
+        const name = user_id+"_router_"+new Date().toFormat("YYYYMMDDHH24MISS");
+        const command = "'ip netns add " + name + "'";
+        console.log(command + "-> " + await execShellCommand(command));
+        const open_id = "router_"+new Date().toFormat("YYYYMMDDHH24MISS");
+        const display_name = "router";
+        const type = "router";
+        const x = req.body.x;
+        const y = req.body.y;
+        await queryExec("insert into machine (open_id, user_id, name, display_name, type, x, y) values ('"
+                        +open_id+"','"+user_id+"','"+name+"','"+display_name+"','"+type+"',"+x+","+y+");");
+        res.json({
+            status: 0,
+            id: open_id,
+            name: display_name,
+            log: "Success: add router on your topology"
         });
-    });
+    } catch(error){
+        console.log(error);
+        res.json(getErrorJson(error));
+    }
 });
 
 //ルータの削除
@@ -63,41 +69,40 @@ router.post('/router/del', function(req, res){
 
 /**
  * スイッチの追加
+ * OpenVSwitchのbridge名は15文字を超えるとエラーを吐くので乱数で生成
  */
-router.post('/switch/add', function(req, res){
-    let userid = req.session.userid;
-    
-    UserMachine.find({"userid" : userid}, function(error, result){
-        if (error)
-            res.json(getErrorJson(error));
-        let userMachine = result[0];
-        let name = userid + userMachine.machines.length;
-        let type = "switch";
-        let x = req.body.x;
-        let y = req.body.y;
-        exec(execPath + " 'ovs-vsctl add-br " + name + "'", function(error, stdout, stderr){
-            if (error)
-                res.json(getErrorJson(error));
-            let l2switch = { name : name, display_name : "switch", type : type, x : x, y : y};
-            userMachine.machines.push(l2switch);
-            let ports = []
-            for (i = 0;i < INITIAL_NUMBER_OF_PORTS_OF_SWITCH;i++){
-                let port = {status: 0, name: "", open_port_number: i};
-                userMachine.machines.push(port);
-                ports.push(port)
-            }
-            userMachine.save(function(error){
-                if (error)
-                    res.json(getErrorJson(error));
-                res.json({
-                    status: 1,
-                    name: name,
-                    port: ports,
-                    log: "success: add switch on your topology"
-                });
-            });
+router.post('/switch/add', async function(req, res){
+    try{
+        const user_id_search = await queryExec("select login_id, user_id from user where login_id='"+req.session.login_id+"';")
+        const user_id = user_id_search[0].user_id;
+        let name = ""; //15文字
+        let search_name;
+        const switch_name_char_set = "abcdefghijklmnopqrstuvwxyz0123456789";
+        do {
+            name = "";
+            for (let i=0;i < 15;i++)
+                name += switch_name_char_set[Math.floor(Math.random()* switch_name_char_set.length)];
+            search_name = await queryExec("select name from machine where name='"+name+"';");
+        } while (search_name.length != 0); //既存のbridge名と重複したら再生性
+        const command = "'ovs-vsctl add-br " + name + "'";
+        console.log(command + "-> " + await execShellCommand(command));
+        const open_id = "switch_"+new Date().toFormat("YYYYMMDDHH24MISS");
+        const display_name = "switch";
+        const type = "switch";
+        const x = req.body.x;
+        const y = req.body.y;
+        await queryExec("insert into machine (open_id, user_id, name, display_name, type, x, y) values ('"
+                        +open_id+"','"+user_id+"','"+name+"','"+display_name+"','"+type+"',"+x+","+y+");");
+        res.json({
+            status: 0,
+            id: open_id,
+            name: display_name,
+            log: "Success: add switch on your topology"
         });
-    });
+    } catch(error){
+        console.log(error);
+        res.json(getErrorJson(error));
+    }
 });
 
 //スイッチの削除
@@ -106,29 +111,28 @@ router.post('/switch/del', function(req, res){
 });
 
 //仮想マシンの追加
-router.post('/vm/add', function(req, res){
-    let userid = req.session.userid;
-    
-    UserMachine.find({"userid" : userid}, function(error, result){
-        if (error)
-            res.json(getErrorJson(error));
-        let userMachine = result[0];
-        let name = userid + userMachine.machines.length;
-        let type = "vm";
-        let x = req.body.x;
-        let y = req.body.y;
-        let vm = { name : name, type : type, x : x, y : y};
-        result[0].machines.push(vm);
-        result[0].save(function(error){
-            if (error)
-                res.json(getErrorJson(error));
-            res.json({
-                status: 1,
-                name: name,
-                log: "success: add vm on your topology"
-            });
+router.post('/vm/add', async function(req, res){
+    try{
+        const user_id_search = await queryExec("select login_id, user_id from user where login_id='"+req.session.login_id+"';")
+        const user_id = user_id_search[0].user_id;
+        const name = user_id+"_vm_"+new Date().toFormat("YYYYMMDDHH24MISS");
+        const open_id = "vm_"+new Date().toFormat("YYYYMMDDHH24MISS");
+        const display_name = "vm";
+        const type = "vm";
+        const x = req.body.x;
+        const y = req.body.y;
+        await queryExec("insert into machine (open_id, user_id, name, display_name, type, x, y) values ('"
+                        +open_id+"','"+user_id+"','"+name+"','"+display_name+"','"+type+"',"+x+","+y+");");
+        res.json({
+            status: 0,
+            id: open_id,
+            name: display_name,
+            log: "Success: add vm on your topology"
         });
-    });
+    } catch(error){
+        console.log(error);
+        res.json(getErrorJson(error));
+    }
 });
 
 //仮想マシンの削除
@@ -136,9 +140,11 @@ router.post('/vm/del', function(req, res){
 
 });
 
-//vethの追加
-router.post('/veth/add', function(req, res){
-
+/**
+ * vethの追加
+ */
+router.post('/veth/add', async function(req, res){
+    
 });
 
 //vethの削除
